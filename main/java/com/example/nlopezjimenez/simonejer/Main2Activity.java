897205ae6e836d74273b739db8f61a19dd8477dd
nlopezjimenez <naborlopezjimenez@gmail.com>
2016-12-16 12:15:58 +0100

    Añadido fragment y segunda activity

---
+++ main/java/com/example/nlopezjimenez/simonejer/Main2Activity.java
@@ -0,0 +1,7 @@
+package com.example.nlopezjimenez.simonejer;
+
+/**
+ * Created by nlopezjimenez on 16/12/16.
+ */
+public class Main2Activity {
+}
---
+++ main/java/com/example/nlopezjimenez/simonejer/fragment_emergente.java
@@ -0,0 +1,31 @@
+package com.example.nlopezjimenez.simonejer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link fragment_emergente.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link fragment_emergente#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragment_emergente extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       
+        return inflater.inflate(R.layout.fragmentemergente, container, false);
+    }
+
+
+}
