a6c0cf1fe3c42a7f4ec25c075cb9355654530b67
nlopezjimenez <naborlopezjimenez@gmail.com>
2016-12-16 12:25:24 +0100

    Determinado el setContentView en el main2activity

--- main/java/com/example/nlopezjimenez/simonejer/Main2Activity.java
+++ main/java/com/example/nlopezjimenez/simonejer/Main2Activity.java
@@ -1,7 +1,18 @@
 package com.example.nlopezjimenez.simonejer;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by nlopezjimenez on 16/12/16.
  */
-public class Main2Activity {
+public class Main2Activity extends AppCompatActivity {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_emerg);
+    }
 }
